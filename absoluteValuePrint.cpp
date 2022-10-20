37eadf85da8b54e090e4ec41c631c41281cc93a6
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2022-10-20 22:48:09 +0530

    Create Absolute value

---
+++ absoluteValuePrint.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cout<<"ENter the value"<<endl;
+    cin>>a;
+   
+    
+    if(a<0) {
+        cout<<-a<<endl;
+    }
+    else{
+        cout<<a<<endl;
+    }
+    
+    
+}
